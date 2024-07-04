86c04123278c0c93f9a05eda4f353f2d4c010567
brunobandeiraf <brunobandeirafernandes@gmail.com>
2024-07-04 13:21:22 -0300

    H2

--- src/main/java/br/com/brunobandeiraf/todolist/user/UserModel.java
+++ src/main/java/br/com/brunobandeiraf/todolist/user/UserModel.java
@@ -1,13 +1,31 @@
 package br.com.brunobandeiraf.todolist.user;
 
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.type.descriptor.java.LocalDateTimeJavaDescriptor;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 // @Data - Getters and Setters
+@Entity(name = "tb_users")
 public class UserModel {
     
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(name = "username")
     private String username;
     private String name;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTimeJavaDescriptor createdAt;
 
 }
